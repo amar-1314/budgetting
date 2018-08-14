b5cc2da9d2a6c8ccc87cc19f5aea321e4e0fde21
AMARNATH MACHA <amar@AMARNATHs-MacBook-Pro.local>
2018-08-13 21:03:45 -0400

    initial commit git

---
+++ src/main/java/com/learnings/spring/budgetting/IndexController.java
@@ -0,0 +1,18 @@
+package com.learnings.spring.budgetting;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+    @RequestMapping("/")
+    public String home() {
+        return "index.jsp";
+    }
+
+    @RequestMapping("/feedback.htm")
+    public String feedback(){
+        return "feedback.jsp";
+    }
+
+}
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,18 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: amar
+  Date: 8/12/18
+  Time: 10:12 PM
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Index</title>
+</head>
+<body>
+This is me Amar 10:31!
+<br>
+<a href="feedback.jsp">Feedback here</a>
+</body>
+</html>
